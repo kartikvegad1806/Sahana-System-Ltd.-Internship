eeee21ea2c2083ba1e6998e3732086e4824ba538
Anurag Soliya <anuragsoliya1@gmail.com>
2026-02-24 10:14:22 +0530

    Add files via upload

---
+++ Anurag Soliya/core-python/FIle IO/main.py
@@ -0,0 +1,15 @@
+from functions import *
+
+main_list = inp()
+
+file_name = write_data(main_list)
+
+read_data(file_name)
+
+countlines(file_name)
+
+countwords(file_name)
+
+countchar(file_name)
+
+countcharwithspace(file_name)
