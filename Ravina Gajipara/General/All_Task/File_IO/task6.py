4b2c637f0b906cbabefb7c5cf18a22fccdfffba3
Ravina Gajipara <161796519+RavinaGajipara@users.noreply.github.com>
2026-02-25 00:12:50 -0800

    Update task6.py

--- Ravina Gajipara/General/All_Task/File_IO/task6.py
+++ Ravina Gajipara/General/All_Task/File_IO/task6.py
@@ -1,49 +1,46 @@
-def file_process(demo_path, dummy_path):
+def write_data(filepath):
+        # Step 1: Take number of lines- use 3 dif fun 
+        n = int(input("Enter number of lines: "))
 
-    # Step 1: Take number of lines
-    n = int(input("Enter number of lines: "))
+        # Step 2: Write data to file
+        with open(filepath, "a") as f:
+            for i in range(n):
+                line = input(f"Enter line {i+1}: ")
+                f.write(line + "\n")
+        print("\nData written successfully!\n")
 
-    # Step 2: Write data in demo.txt
-    with open(demo_path, "w") as f:
-        for i in range(n):
-            line = input(f"Enter line {i+1}: ")
-            f.write(line + "\n")
+def reverse_lines(demo_file, dummy_path):
+        # Step 3: Reverse line order and write in dummy.txt
+        with open(demo_file, "r") as f:
+            lines = f.readlines()
 
-    print("\nData written to demo.txt")
+        reversed_lines = lines[::-1]
 
-    # Step 3: Read data from demo.txt
-    with open(demo_path, "r") as f:
-        lines = f.readlines()
+        with open(dummy_path, "w") as f:
+            f.writelines(reversed_lines)
 
-    print("\nContent of demo.txt:")
-    for line in lines:
-        print(line.strip())
+        print("\nLine-wise reversed data written to dummy.txt")
 
-    # Step 4: Reverse line order and write in dummy.txt
-    reversed_lines = lines[::-1]
+def replace_word(dummy_path):
+        # Step 4: Replace word in dummy.txt
+        old_word = input("\nEnter word to replace: ")
+        new_word = input("Enter replacement word: ")
 
-    with open(dummy_path, "w") as f:
-        f.writelines(reversed_lines)
+        with open(dummy_path, "r") as f:
+            content = f.read()
 
-    print("\nLine-wise reversed data written to dummy.txt")
+        updated_content = content.replace(old_word, new_word)
 
-    # Step 5: Replace word in dummy.txt
-    old_word = input("\nEnter word to replace: ")
-    new_word = input("Enter replacement word: ")
+        with open(dummy_path, "w") as f:
+            f.write(updated_content)
 
-    with open(dummy_path, "r") as f:
-        content = f.read()
-
-    updated_content = content.replace(old_word, new_word)
-
-    with open(dummy_path, "w") as f:
-        f.write(updated_content)
-
-    print("\nWord replaced successfully in dummy.txt")
+        print("\nWord replaced successfully in dummy.txt")
 
 
 # User passes file paths
 demo_file = input("Enter full path for demo.txt: ")
 dummy_file = input("Enter full path for dummy.txt: ")
 
-file_process(demo_file, dummy_file)
+write_data(demo_file)
+reverse_lines(demo_file, dummy_file)
+replace_word(dummy_file)
